a1542c64b5fba7c5794d0be078a7280192c762a6
Ranjith <mailranjith7022@gmail.com>
2024-10-31 13:06:17 +0530

    Filter Messages examples

--- main_test.py
+++ main_test.py
@@ -1,28 +1,56 @@
 from main import *
 
 run_cases = [
-    (4, 4),
-    (6, 9),
+    (
+        ["darn it", "this dang thing won't work", "lets fight one on one"],
+        ["darn it", "this thing won't work", "lets fight one on one"],
+        [0, 1, 0],
+    ),
 ]
 
 submit_cases = run_cases + [
-    (0, 0),
-    (1, 0),
-    (2, 1),
-    (4, 4),
-    (10, 25),
-    (15, 49),
+    (
+        [
+            "well dang it",
+            "dang the whole dang thing",
+            "kill that knight, dang it",
+            "get him!",
+            "donkey kong",
+            "oh come on, get them",
+            "run away from the dang baddies",
+        ],
+        [
+            "well it",
+            "the whole thing",
+            "kill that knight, it",
+            "get him!",
+            "donkey kong",
+            "oh come on, get them",
+            "run away from the baddies",
+        ],
+        [1, 2, 1, 0, 0, 0, 1],
+    ),
 ]
 
 
-def test(input1, expected_output):
+def test(input, expected_output1, expected_output2):
     print("---------------------------------")
-    print(f"Inputs:")
-    print(f" * end: {input1}")
-    print(f"Expecting: {expected_output}")
-    result = sum_of_odd_numbers(input1)
-    print(f"Actual: {result}")
-    if result == expected_output:
+    print(f"Input:")
+    print(f" * messages: {input}")
+    print("Expecting:")
+    print(f" * filtered messages: {expected_output1}")
+    print(f" * words removed: {expected_output2}")
+    print("Actual:")
+    try:
+        result = filter_messages(input)
+        print(f" * filtered messages: {result[0]}")
+        print(f" * words removed: {result[1]}")
+    except Exception as e:
+        print(f"Error: {e}")
+        print("Fail")
+        return False
+
+    if result == (expected_output1, expected_output2):
         print("Pass")
         return True
     print("Fail")
