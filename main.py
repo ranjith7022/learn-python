c7af887e6e9c77e72f3b820ae84ffa12518c9425
ranjith <73107931+ranjith7022@users.noreply.github.com>
2024-10-31 13:11:15 +0530

    Filter Messages examples
    Filter Messages examples

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
-def sum_of_odd_numbers(end):
-    total = 0
-    for i in range(1, end):
-        if i%2 == 1:
-           total += i
-    return total
+def filter_messages(messages):
+    filter_messages =[]
+    count = [0]*len(messages)
+    for i in range(len(messages)):
+        if("dang"in messages[i].lower()):
+            count[i]= messages[i].count("dang")
+            filter_messages.append(" ".join([word.strip() for word in messages[i].split("dang") if word.strip()]))
+            
+        else:
+            filter_messages.append(messages[i])
+
+
+    
+    return filter_messages,count
+  
