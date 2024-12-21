117f112384a271135a7fd69c72a61e4227f2ad30
Ranjith <mailranjith7022@gmail.com>
2024-12-21 18:19:54 +0530

    sum_nested_list updated

--- main.py
+++ main.py
@@ -1,15 +1,8 @@
-# def sum_nested_list(lst):
-#     if len(lst) == 0:
-#         return 0
-#     return sum(i if isinstance(i, int) else sum_nested_list(i) for i in lst )
-
 def sum_nested_list(lst):
-    if isinstance(lst,list) and len(lst) == 0 :
-        return 0
-    if isinstance(lst,list) and len(lst) > 1:
-        return sum_nested_list(lst[0]) + sum_nested_list(lst[1:])
-    elif isinstance(lst,int):
-        return lst
-    elif isinstance(lst,list) and len(lst) == 1:
-        return sum_nested_list(lst[0])
-    
+    count = 0
+    for item in lst:
+        if isinstance(item, list):
+            count += sum_nested_list(item)
+        else:
+            count += item
+    return count
--- sum_nested_list.py
+++ sum_nested_list.py
@@ -13,3 +13,7 @@ def sum_nested_list(lst):
     elif isinstance(lst,list) and len(lst) == 1:
         return sum_nested_list(lst[0])
     
+    import functools
+
+    def sum_nested_list(lst):
+        return functools.reduce(lambda sum, item: sum + item if isinstance(item, int) else sum + sum_nested_list(item), lst, 0)
