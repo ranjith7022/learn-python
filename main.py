d734378e903ed526bea3f290ec50d87ad39d16a2
Ranjith <mailranjith7022@gmail.com>
2025-02-01 20:04:09 +0530

    github actions

--- main.py
+++ main.py
@@ -10,26 +10,27 @@ class EditType(Enum):
 
 def handle_edit(document, edit_type, edit):
     match edit_type:
-        
+
         case EditType.NEWLINE:
             value = document.split("\n")
-            value.insert(edit['line_number']+1,'')
+            value.insert(edit['line_number']+1, '')
             return "\n".join(value)
         case EditType.INSERT:
             value = document.split("\n")
-            value.insert(edit['line_number'],edit["insert_text"])
-            value.pop()
+            value.insert(edit['line_number'], edit["insert_text"])
+            #value.pop()
             return "\n".join(value)
         case EditType.DELETE:
             value = document.split("\n")
-            value[edit["line_number"]] = value[edit["line_number"]][:edit["start"]] + value[edit["line_number"]][edit["end"]:]  #+   
+            value[edit["line_number"]] = value[edit["line_number"]
+                                               ][:edit["start"]] + value[edit["line_number"]][edit["end"]:]  # +
             return "\n".join(value)
         case EditType.SUBSTITUTE:
             value = document.split("\n")
-            value[edit["line_number"]] = value[edit["line_number"]][:edit["start"]] + edit["insert_text"]+ value[edit["line_number"]][edit["end"]:]  #+   
+            value[edit["line_number"]] = value[edit["line_number"]][:edit["start"]] + \
+                edit["insert_text"] + \
+                value[edit["line_number"]][edit["end"]:]  # +
             return "\n".join(value)
-            
+
         case _:
             raise Exception("Unknown edit type")
-            
-        
